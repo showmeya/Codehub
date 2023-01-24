b17842def94bca12b457cbe4dd702cc2f5f49a7f
showmeya <sowmevijay67@gmail.com>
2023-01-24 09:38:19 +0530

    Course details

---
+++ src/test/java/com/hexa/CourseDetails.java
@@ -0,0 +1,17 @@
+package com.hexa;
+
+public class CourseDetails {
+	public void javaCourse() {
+		System.out.println("java course started" );
+		}
+	public void testCourse() {
+		System.out.println("testing started");
+	}
+	public void python() {
+		System.out.println("python started");
+
+	}
+}
+
+
+
